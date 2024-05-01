484b44029741e05f0f17db56dde62e5fdbc04cff
DAHAN7 <ekgks4843@gmail.com>
2024-05-01 14:17:28 +0900

    first commit

---
+++ 워크벤치 사용팁.sql
@@ -0,0 +1,9 @@
+-- 워크벤치 사용 팁
+use shopdb;
+
+seLECt * from MEMBERtBl;
+-- ctrl + D : 한 줄 복제
+-- 예약어 대문자,소문자로 변경
+-- Edit -Format - Beautify Query
+-- Edit -Format - Beautify Query
+select * from membertbl;
