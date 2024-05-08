d368d1a3d8fcd4b84f4a08783e6aadd009f00571
DAHAN7 <ekgks4843@gmail.com>
2024-05-08 16:28:52 +0900

    first commit

--- 테이블생성/연습문제4테이블구조변경.sql
+++ 테이블생성/연습문제4테이블구조변경.sql
@@ -60,8 +60,12 @@ WHERE TABLE_SCHEMA = 'exercise';
 
 ALTER TABLE projects
 	DROP CONSTRAINT projects_chk_1;
-
 -- 연습 문제 15: assignments 테이블에서 동일한 직원과 프로젝트 조합이 중복되지 않도록 하는 제약조건을 삭제하세요. (제약조건 이름 확인 필요)
+-- 외래키 제약조건이 삭제가 전제되어야 함.
+ALTER TABLE assignments
+	DROP CONSTRAINT assignments_ibfk_1;
+ALTER TABLE assignments
+	DROP CONSTRAINT assignments_ibfk_2;
 ALTER TABLE assignments
 	DROP CONSTRAINT emp_id;
 
